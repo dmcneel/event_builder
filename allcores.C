7ee17b6eccea9b526c5c4c974edabcc6c86ee1e8
Daniel McNeel <daniel.g.mcneel@gmail.com>
2018-06-27 11:40:13 -0400

    updated allcores

--- allcores.C
+++ allcores.C
@@ -7,7 +7,7 @@ void allcores(Int_t RUNNUM){
   // gROOT->ProcessLine(".L sort3.C++g");
   TChain *chain=new TChain("tree");
   chain->Add(name);
-   TProof::Open("workers=14");
+   TProof::Open("");
     chain->SetProof();
     chain->Process("builder.C+","of=gen.root;stf",50000000);
 }
